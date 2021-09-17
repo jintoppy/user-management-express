2fe702fdec75b430174bbc68fa8ca9f662836915
Jinto Jose <jintoppy@gmail.com>
2021-09-17 11:01:45 +0530

    session

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,43 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { users } from './data/users';
 import apiRouter from './routes';
+import session, { SessionData } from 'express-session';
+
+declare module 'express-session' {
+    interface SessionData {
+        views: number;
+        title: string;
+    }
+}
 
 import { json } from 'body-parser';
 
 const app: Application = express();
 
+
+app.use(session({
+    secret: 'my secret',
+    cookie: {}
+}));
+
 app.use(json());  //req.body
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
 app.use((req, res, next) => {
-    console.log(req.url);
+    console.log(req.url);    
+    console.log()
     next();
 });
 
-app.get('/users', (req: Request, res: Response) => {
+app.get('/users', (req: Request, res: Response) => {  
+    req.session.views = req.session.views ? req.session.views + 1: 1;   
+    console.log(req.session.views);
     res.render('users', {
         title: 'MEAN',
-        users
+        users,
+        views: req.session.views
     });
 });
 
