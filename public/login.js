a9efcc6e460f385a849aed3e5af283cac4867c95
Jinto Jose <jintoppy@gmail.com>
2021-09-17 11:47:09 +0530

    login page

---
+++ public/login.js
@@ -0,0 +1,9 @@
+const usernameTxt = document.querySelector('#username');
+const passwordTxt = document.querySelector('#password');
+const loginBtn = document.querySelector('#loginBtn');
+
+loginBtn.addEventListener('click', () => {
+    const username = usernameTxt.value;
+    const password = passwordTxt.value;
+    
+});
