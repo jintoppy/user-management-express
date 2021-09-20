87ce51abe5409089450200be4fe6150bcd21e095
Jinto Jose <jintoppy@gmail.com>
2021-09-20 09:29:12 +0530

    login

--- public/login.js
+++ public/login.js
@@ -2,10 +2,21 @@ const usernameTxt = document.querySelector('#username');
 const passwordTxt = document.querySelector('#password');
 const loginBtn = document.querySelector('#loginBtn');
 
-loginBtn.addEventListener('click', () => {
+loginBtn.addEventListener('click', async (evnt) => {
+    evnt.preventDefault();
     const username = usernameTxt.value;
     const password = passwordTxt.value;
-    fetch('', {
-        method: 'POST'
-    })
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })      
+    });
+    const { token } = await response.json();
+    localStorage.setItem('token', token);
+    location.href = '/';
 });
