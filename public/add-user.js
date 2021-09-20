9dea023a5d870ec806fdbdfc43c59a6659d86dd9
Jinto Jose <jintoppy@gmail.com>
2021-09-20 09:44:50 +0530

    adding user

--- public/add-user.js
+++ public/add-user.js
@@ -2,7 +2,34 @@ const nameTxt = document.querySelector('#name');
 const industryTxt = document.querySelector('#industry');
 const addBtn = document.querySelector('#addBtn');
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
     const name = nameTxt.value;
     const industry = industryTxt.value;     
+    const response = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'token': localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            name,
+            industry
+        })      
+    });
+    try {
+        const res = await response.json();
+        if(res.status === 200){
+            location.href = '/users';
+        }
+        else {
+            document.querySelector('#msg').innerHTML = res.msg;            
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+    
+    
+    
 });
